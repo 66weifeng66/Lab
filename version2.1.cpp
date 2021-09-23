c2ef82c45496fe84c2d3c6272b35e47d2cc662d8
66weifeng66 <90778119+66weifeng66@users.noreply.github.com>
2021-09-23 21:46:23 +0800

    version 3.1

--- version2.1.cpp
+++ version2.1.cpp
@@ -3,7 +3,7 @@
 #include <stack>
 using namespace std;
 int main ()
-{
+{ 
    FILE *fp;
    char c;
    int i,cnt=0,cnt1=0,cnt2=0,ss[100];
@@ -12,7 +12,7 @@ int main ()
    string ans[100];
    stack<char>s;
    char file_path[100];
-   cout<<"请输入文件路径：";
+   cout<<"Please enter the file path：";
    cin>>file_path;
    string a[35]={"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
    fp = fopen(file_path,"r");
@@ -58,5 +58,7 @@ int main ()
    for(i=0;i<j;i++){
    	cout<<ss[i]<<" ";
    }
-   
 }
+
+   
+   
